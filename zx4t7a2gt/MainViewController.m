8855d8cb35b232ec320052fe17b2b76fb1d6ac73
endogenesis <user@Mac-mini-User-2.local>
2015-01-26 18:03:14 +0300

    after work day

--- zx4t7a2gt/MainViewController.m
+++ zx4t7a2gt/MainViewController.m
@@ -9,6 +9,7 @@
 #import "MainViewController.h"
 
 @interface MainViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -16,12 +17,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(processImage)];
+    
+    UIImage *testIm= [UIImage imageNamed:@"testImage"];
+    [self.imageView setContentMode:UIViewContentModeScaleAspectFit];
+    [self.imageView setImage:testIm];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) processImage {
+    
 }
 
 @end
